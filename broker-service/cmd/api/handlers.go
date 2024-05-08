01023d0146054b232899282506f4f74e27f59519
argonautts <sasha_kozin2020@mail.ru>
2024-05-09 02:07:37 +0300

    Updating the front end to send mail

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -154,7 +154,7 @@ func (app *Config) sendMail(w http.ResponseWriter, msg MailPayload) {
 	jsonData, _ := json.MarshalIndent(msg, "", "\t")
 
 	// call the mail service
-	mailServiceURL := "http://mail-service/send"
+	mailServiceURL := "http://mailer-service/send"
 
 	// post to mail service
 	request, err := http.NewRequest("POST", mailServiceURL, bytes.NewBuffer(jsonData))
@@ -164,6 +164,7 @@ func (app *Config) sendMail(w http.ResponseWriter, msg MailPayload) {
 	}
 
 	request.Header.Set("Content-Type", "application/json")
+
 
 	client := &http.Client{}
 	response, err := client.Do(request)
@@ -185,4 +186,5 @@ func (app *Config) sendMail(w http.ResponseWriter, msg MailPayload) {
 	payload.Message = "Message sent to " + msg.To
 
 	app.writeJSON(w, http.StatusAccepted, payload)
+
 }
